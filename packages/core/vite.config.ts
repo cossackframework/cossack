4d986ddf0a580d011d28a4bb7809acb9ea13ed3e
Tan Nguyen <hi@tan.ng>
2025-10-05 16:16:46 +0000

    path alias

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
-    })
+    }),
+    tsconfigPaths(),
   ],
   build: {
     lib: {
--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
@@ -16,5 +17,6 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    tsconfigPaths(),
   ],
 });
