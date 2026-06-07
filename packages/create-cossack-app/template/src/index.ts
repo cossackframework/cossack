6f47c6b61a5eac13a3c58e49f1c9b8606906159b
Tan Nguyen <hi@tan.ng>
2026-06-07 06:28:17 +0000

    refactor: update cca template

--- packages/create-cossack-app/template/src/index.ts
+++ packages/create-cossack-app/template/src/index.ts
@@ -4,8 +4,9 @@ import 'reflect-metadata';
 import { createApp } from '@cossackframework/framework/router';
 import { AppDurableObject } from '@cossackframework/framework/DurableObject';
 import { App } from './App';
+import { template } from './root';
 
-const app = createApp({ AppComponent: App });
+const app = createApp({ AppComponent: App, template });
 
 export { AppDurableObject };
 export default {
---
+++ packages/create-cossack-app/template/src/root.ts
@@ -0,0 +1,12 @@
+export const template = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        {{ cossackScripts }}
+    </head>
+    <body class="antialiased">
+        {{ cossackBody }}
+    </body>
+</html>
+`;
