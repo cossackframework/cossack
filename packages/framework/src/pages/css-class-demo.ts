dc3c8ed70954d609953538dc308b1cb10d5504b7
Tan Nguyen <hi@tan.ng>
2026-06-28 15:57:11 +0700

    refactor: update types

--- packages/framework/src/pages/css-class-demo.ts
+++ packages/framework/src/pages/css-class-demo.ts
@@ -1,7 +1,5 @@
-import type { MiddlewareHandler } from 'hono';
-import { Button } from '@/components/Button';
-import { html, type TemplateResult, component, classMap } from '@cossackframework/renderer';
-import { Cossack, isServer, Page, Server, State, HeadTag, HeadContext, HeadValue } from '@cossackframework/core';
+import { Cossack, Page } from '@cossackframework/core';
+import { classMap, html, type TemplateResult } from '@cossackframework/renderer';
 
 @Page()
 export class CSSClassDemo extends Cossack {
@@ -10,8 +8,8 @@ export class CSSClassDemo extends Cossack {
         const backgroundClass = 'bg-red-500';
         const dynamicClasses = classMap({
             'bg-red-500': true,
-            'h-[100px]': true,
-            'w-[100px]': true,
+            'h-25': true,
+            'w-25': true,
         });
 
         return html`
@@ -19,7 +17,7 @@ export class CSSClassDemo extends Cossack {
             
             <div class="flex">
                 <!-- This is working at SSR time, but not at CSR time -->
-                <div class="h-[100px] w-[100px] ${backgroundClass}"></div>
+                <div class="h-25 w-25 ${backgroundClass}"></div>
                 
                 <!-- This is working at both SSR and CSR time -->
                 <div class="${dynamicClasses}"></div>
