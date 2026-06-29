6774b9bd6f3917f161e3617b7003f9c795f03c4e
Tan Nguyen <hi@tan.ng>
2026-06-29 09:07:56 +0700

    feat: add viewport and antialiased class

--- packages/create-cossack-app/template/src/root.ts
+++ packages/create-cossack-app/template/src/root.ts
@@ -3,6 +3,7 @@ export const template = `
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         {{ cossackScripts }}
     </head>
     <body class="antialiased">
--- packages/framework/src/root.ts
+++ packages/framework/src/root.ts
@@ -115,9 +115,10 @@ export const renderRoot = (props: RenderRootProps) => {
         <html lang="en">
             <head>
                 <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 ${cossackScripts()}
             </head>
-            <body>
+            <body class="antialiased">
                 ${cossackBody()}
             </body>
         </html>
